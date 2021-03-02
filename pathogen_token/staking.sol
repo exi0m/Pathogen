583bb64e2ebd0c3c6c57e86ab89341293dd2298a
gqueval <gabrielqv@berkeley.edu>
2021-03-02 15:58:50 -0800

    Gabriel Staking

--- pathogen_token/staking.sol
+++ pathogen_token/staking.sol
@@ -1,2 +1,23 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+* @title Pathogen Token (PATH)
+* @notice Implements a basic ERC20 staking token with incentive distribution.
+*/
+contract StakingToken is ERC20, Ownable {
+   using SafeMath for uint256;
+
+   /**
+    * @notice The constructor for the Staking Token.
+    * @param _owner The address to receive all tokens on construction.
+    * @param _supply The amount of tokens to mint on construction.
+    */
+   constructor(address _owner, uint256 _supply)
+       public
+   {
+       _mint(_owner, _supply);
+   }
