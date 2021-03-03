bc46995d9764900e101f9199fe42c31cbf6833d7
Satheesh <satheeshnarasimman@gmail.com>
2021-03-03 07:03:53 -0800

    rewards- changed variables, added comments, removed typos

--- pathogen_token/rewards.sol
+++ pathogen_token/rewards.sol
@@ -1,91 +1,80 @@
 pragma solidity ^0.5.0;
 
-    /**
-    * @notice The accumulated rewards for each Token.
-    */
+    /*The accumulated rewards for each Token */
     
+    /* A rewards mapping object is created */
     mapping(address => uint256) internal rewards;
     
-    /**
-    * @notice A method to allow  pathogen to check his rewards.
-    * @param _pathogen The pathogen to check rewards for.
-    */
-    
-    function rewardOf(address _pathogen)
+    /* Define a function that will take an address and
+    pass it to the rewards mapping object. Any stakeholder
+    can check their rewards in this function */
+
+    function check_rewards(address _pathogen)
         public
         view
-        returns(uint256)
-        
+        returns(uint256)      
     {
-        return rewards(_pathogen);
-        
-    }
-    
-    
-    /**
-    * @notice A method to the aggregated rewards from all Tokens.
-    * @return uint256 The aggregated rewards from all Tokens.
-    */
-    
-    function totalRewards()
+        return rewards(_pathogen);      
+    }  
+
+    /* Calcualte the aggregated rewards from all tokens
+    and return the aggregated rewards */
+
+    function total_rewards()
         public
         view
-        returns(uint256)
-        
+        returns(uint256)    
     {
-        uint256 _totalRewards = 0;
-        for (uint256 t = 0; s < pathogen.length t += 1){
-            _totalRewards = _totalRewards.add(rewards[pathogen[t]]);
+        /* the reward for the pathogen token is 0.2 percent
+        use path_reward as the variable to iterate over the pathogen array */
+
+        uint256 _aggregated_reward = 0;
+        for (uint256 path_reward = 0; path_reward < pathogen.length; path_reward += 0.2)
+        {
+            _aggregated_reward = _aggregated_reward.add(rewards[pathogen[path_reward]]);
             
-        }
-        
-        return _totalRewards;
+        }     
+        return _aggregated_reward;
     }
         
-    /**
-    * @notice A simple method that calculates the rewards for each Token.
-    * @param _pathogen The pathogen to calculate rewards for.
-    */
+    /* calculate the rewards for each Token */
     
-    function calculateReward(address _pathogen)
+    function calculate_reward(address _pathogen)
         public
         view
-        returns(uint256)
-        
+        returns(uint256)  
     {
-        returns Tokens(_pathogen) /100;
+        return stake_record(_pathogen) /100;
         
-    }
+    } 
     
-    /**
-    * @notice A method to distribute rewards to all stakeholders.
-    */
-    function distributeRewards()
-        public
-        onlyOwner
-        
+    /* Distribute rewards to all Tokens 
+    @steven: this is confusing. We iterate over the length of the pathogen array to add 
+    all the stakeholders to whom the rewards need to be distributed. 
+    
+    How does 0.2 come in here? */
+    function distribute_rewards() public only_owner       
     {
-        for (uint256 t = 0; t < pathogen.tength t += 1){
-            address pathogen = pathoen[t];
-            uint256 reward = calculateReward(pathoen);
-            rewards[pathogen] = rewards[pathogen].add(reward);
+        for (uint256 path_reward= 0; path_reward < pathogen.length; path_reward += 0.2)
+        {
+            /* for all the addresses in the pathogen array, distribute a 0.2 percent reward */
+            address rewarding_address = pathogen[path_reward];
+            uint256 give_reward = calculate_reward(rewarding_address);
+
+            /* all the addresses that receive the reward are added
+            to the 'rewards' mapping object */
+            rewards[rewarding_address] = rewards[rewarding_address].add(give_reward);
         }
     }
     
-    /**
-    * @notice A method to allow a stakeholder to withdraw his rewards.
+    /* function to withdraw stakeholder's rewards.
     */
     
-    function withdrawReward()
-        public
-        
+    function withdraw_reward()
+        public       
     {
-        uint256 reward = rewards[msg.sender];
+        uint256 rewarding_address = rewards[msg.sender];
         rewards[msg.sender] = 0;
-        _mint(msg.sender, reward);
+        _mint(msg.sender, rewarding_address);
         
     }
-    
-    
-       
-    
